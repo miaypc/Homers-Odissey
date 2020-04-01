3f434a83df764b40f8ddd9308fc1825fa38c5a63
Mia Yin <me@miayin.com>
2020-04-01 12:27:38 +0200

    email input

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div>
+      <h1>Email Input</h1>
+      <input type="email" name="email" />
+    </div>
+  );
+}
+
+export default SignUp;
